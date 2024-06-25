bb9a3ccc9216853d3e8f8961bd793f04c8b102da
Taha Hamada <taha2901>
2024-06-25 19:31:52 +0300

    .......

--- lib/features/appointments/presentation/manager/add_medicine_cubit/add_medicine_cubit.dart
+++ lib/features/appointments/presentation/manager/add_medicine_cubit/add_medicine_cubit.dart
@@ -1,52 +1,10 @@
-// import 'package:bloc/bloc.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:gluco/core/helper/api.dart';
-// import 'package:gluco/core/widgets/network.dart';
-// import 'package:gluco/features/appointments/data/add_medicine_model.dart';
-// import 'package:meta/meta.dart';
-
-// part 'add_medicine_state.dart';
-
-// class AddMedicineCubit extends Cubit<AddMedicineState> {
-//   AddMedicineCubit() : super(AddMedicineInitial());
-
-//   static AddMedicineCubit get(context) => BlocProvider.of(context);
-
-//   void addMedicine({
-//   required String name,
-//   required String dosage,
-//   required String times,
-//   required String date,
-// }) {
-//   AddMedicineModel? addMedicineModel;
-//   emit(AddMedicineLoaded());
-//   DioHelper().postData(
-//     url: ADD_Medicine,
-//     token: 'Bearer $userToken',
-//     data: {
-//       'name': name,
-//       'dosage': dosage,
-//       'times': times,
-//       'date': date,
-//     },
-//   ).then((value) {
-//     addMedicineModel = AddMedicineModel.fromJson(value.data);
-//     emit(AddMedicineSuccess(addMedicineModel!));
-//     // تأكد من عدم إغلاق Cubit هنا
-//   }).catchError((onError) {
-//     print('Error in Add medicine cubit: ${onError.toString()}');
-//     emit(AddMedicineFailure(onError.toString()));
-//   });
-// }
-
-// }
-
-
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gluco/core/helper/api.dart';
 import 'package:gluco/core/widgets/network.dart';
 import 'package:gluco/features/appointments/data/add_medicine_model.dart';
+import 'package:gluco/features/appointments/presentation/manager/get_medicine_cubit/get_medicine_cubit.dart';
 import 'package:meta/meta.dart';
 
 part 'add_medicine_state.dart';
@@ -54,15 +12,15 @@ part 'add_medicine_state.dart';
 class AddMedicineCubit extends Cubit<AddMedicineState> {
   AddMedicineCubit() : super(AddMedicineInitial());
 
-  static AddMedicineCubit get(context) => BlocProvider.of(context);
+  static AddMedicineCubit get(BuildContext context) => BlocProvider.of(context);
 
   void addMedicine({
     required String name,
     required String dosage,
     required String times,
     required String date,
+    required BuildContext context,
   }) {
-    emit(AddMedicineLoaded());
     DioHelper().postData(
       url: ADD_Medicine,
       token: 'Bearer $userToken',
@@ -71,10 +29,14 @@ class AddMedicineCubit extends Cubit<AddMedicineState> {
         'dosage': dosage,
         'times': times,
         'date': date,
+        
       },
     ).then((value) {
       AddMedicineModel addMedicineModel = AddMedicineModel.fromJson(value.data);
       emit(AddMedicineSuccess(addMedicineModel));
+
+      // Move this line here to fetch medicines after adding successfully
+      GetMedicineCubit.get(context).getMedicines();
     }).catchError((onError) {
       emit(AddMedicineFailure(onError.toString()));
     });
--- lib/features/appointments/presentation/view/add_appointments.dart
+++ lib/features/appointments/presentation/view/add_appointments.dart
@@ -1,139 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:gluco/core/widgets/custom_button.dart';
-// import 'package:gluco/core/widgets/custom_show_toast.dart';
-// import 'package:gluco/core/widgets/custom_text_field.dart';
-// import 'package:gluco/features/appointments/presentation/manager/add_medicine_cubit/add_medicine_cubit.dart';
-// import 'package:gluco/features/appointments/presentation/manager/get_medicine_cubit/get_medicine_cubit.dart';
-// import 'package:gluco/features/home/presentation/view/widgets/custom_text_field_center_text.dart';
-
-// class AddAppointments extends StatelessWidget {
-//   const AddAppointments({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-//     var nameController = TextEditingController();
-//     var timesController = TextEditingController();
-//     var dosageController = TextEditingController();
-//     var dateController = TextEditingController();
-
-//     return BlocConsumer<AddMedicineCubit, AddMedicineState>(
-//       listener: (context, state) {
-//         if (state is AddMedicineSuccess) {
-//           showToast(
-//             msg: 'Added Successfully',
-//             state: ToastStates.SUCCESS,
-//           );
-//            GetMedicineCubit.get(context).getMedicines();
-//         } else if (state is AddMedicineFailure) {
-//           showToast(
-//             msg: 'Addition Failed: ${state.errMessage}',
-//             state: ToastStates.ERROR,
-//           );
-//         }
-//       },
-//       builder: (context, state) {
-//         return Scaffold(
-//           appBar: AppBar(
-//             automaticallyImplyLeading: false,
-//             centerTitle: true,
-//             title: const Text('مواعيد الأدويه'),
-//           ),
-//           body: Padding(
-//             padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-//             child: SingleChildScrollView(
-//               child: Form(
-//                 key: formKey,
-//                 child: Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     CustomTextFieldCenterlable(
-//                       showBorder: false,
-//                       hint: 'ابحث عن الدواء',
-//                       suffixIcon: Icons.search,
-//                       controller: nameController,
-//                     ),
-//                     const SizedBox(height: 32),
-//                     Row(
-//                       children: [
-//                         Expanded(
-//                           child: Column(
-//                             children: [
-//                               const SizedBox(height: 16),
-//                               CustomTextField(
-//                                 controller: dosageController,
-//                                 showBorder: false,
-//                                 hint: 'الجرعه',
-//                               ),
-//                             ],
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                     const SizedBox(height: 16),
-//                     Row(
-//                       children: [
-//                         Expanded(
-//                           child: Column(
-//                             children: [
-//                               const SizedBox(height: 16),
-//                               CustomTextField(
-//                                 controller: timesController,
-//                                 showBorder: false,
-//                                 hint: 'الموعد',
-//                               ),
-//                             ],
-//                           ),
-//                         ),
-//                         const SizedBox(width: 8),
-//                         Expanded(
-//                           child: Column(
-//                             children: [
-//                               const SizedBox(height: 16),
-//                               CustomTextField(
-//                                 controller: dateController,
-//                                 showBorder: false,
-//                                 hint: '10.0 pm',
-//                               ),
-//                             ],
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                     SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-//                     state is AddMedicineLoaded
-//                         ? const Center(child: CircularProgressIndicator())
-//                         : CustomButton(
-//                             text: 'إضافه',
-//                             icon: Icons.add,
-//                             color: Colors.black,
-//                             textcolor: Colors.white,
-//                             circular: 10,
-//                             onTap: () {
-//                               if (formKey.currentState!.validate()) {
-//                                 AddMedicineCubit.get(context).addMedicine(
-//                                   dosage: dosageController.text,
-//                                   name: nameController.text,
-//                                   date: dateController.text,
-//                                   times: timesController.text,
-//                                 );
-//                               }
-//                               Navigator.pop(context);
-//                             },
-//                           ),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gluco/core/widgets/custom_button.dart';
@@ -162,6 +26,7 @@ class AddAppointments extends StatelessWidget {
             state: ToastStates.SUCCESS,
           );
           GetMedicineCubit.get(context).getMedicines();
+          Navigator.pop(context);
         } else if (state is AddMedicineFailure) {
           showToast(
             msg: 'Addition Failed: ${state.errMessage}',
@@ -253,9 +118,12 @@ class AddAppointments extends StatelessWidget {
                                   name: nameController.text,
                                   date: dateController.text,
                                   times: timesController.text,
+                                  context: context, // تمرير السياق هنا
                                 );
+
+                                // Remove this line to prevent popping before the medicine is added
                               }
-                              Navigator.pop(context);
+                                
                             },
                           ),
                   ],
