2e800a2d4e1a1ae39bc9bf001fdc5c3fb6be2b1f
Taha Hamada <taha2901>
2024-07-02 03:10:54 +0300

    ...

--- lib/features/pic_products/presentation/view/pic_products.dart
+++ lib/features/pic_products/presentation/view/pic_products.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:gluco/core/widgets/constants.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:image_picker/image_picker.dart';
 
